38ee52fa2c5401e30404f3828df34a4b22ca720e
aufbakanleitung <hvdveer@gmail.com>
2020-01-09 20:09:33 +0100

    the assignments

---
+++ assignment_1.py
@@ -0,0 +1,5 @@
+# Assignment 1
+# create a function that prints all numbers that contain only unique numbers
+numberlist = [2347821, 4567891, 2938479382, 12328827, 837651, 1234567890, 9234793,
+              1823762187, 12372386, 1928379812, 361123, 23456789, 567890, 345678, 4578932]
+
---
+++ assignment_2.py
@@ -0,0 +1,12 @@
+# Assignment 2 - Ciphertext
+# Create a function that encrypts text by moving each letter 2 positions higher in the alphabet
+
+example_text = "In cryptography, encryption is the process of encoding a message or information in such a way that " \
+               "only authorized parties can access it and those who are not authorized cannot."
+
+
+def encrypt(text):
+    pass
+
+
+print(encrypt(example_text))
