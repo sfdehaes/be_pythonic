63d0a3355e134771fe30d7989cc9f44fe185dff3
Herman van der Veer <hvdveer@gmail.com>
2020-06-13 13:19:41 +0200

    add assignment 0

---
+++ assignment_0.py
@@ -0,0 +1,3 @@
+# Create a FizzBuzz
+# Print the number from 1 to 100 but if it's a multiple of 3 print Fizz, of 5 print Buzz, if it's both print FizzBuzz
+# Extra credit; create one that also handles 7 (Balls) and 11 (Booze)
